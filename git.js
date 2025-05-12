7fb2719d9e567f554b3d1b55be45466612c33120
PROFF-1 <richmondnyamedor04@gmail.com>
2025-05-12 11:40:08 +0000

    Add a new code for the feature

--- git.js
+++ git.js
@@ -10,3 +10,6 @@ console.log(gretting('Kilo')); // Hello Kilo
 console.log(gretting(5)); // 10
 
 console.log('Hello This was added to the feature branch'); // Hello This was added to the feature branch
+
+
+console.log('we want to see this code in the feature branch'); // we want to see this code in the feature branch
